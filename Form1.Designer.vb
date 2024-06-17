d0deee483ab74bcf4fd7124de05beb752c856a5a
tomas <tomas@LAPTOP-HQ42VN2G>
2024-06-17 19:20:12 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMinimaUnidad = New System.Windows.Forms.Label()
+        Me.cmdEjecutar = New System.Windows.Forms.Button()
+        Me.txtIngresar = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblMinimaUnidad
+        '
+        Me.lblMinimaUnidad.AutoSize = True
+        Me.lblMinimaUnidad.Location = New System.Drawing.Point(9, 47)
+        Me.lblMinimaUnidad.Name = "lblMinimaUnidad"
+        Me.lblMinimaUnidad.Size = New System.Drawing.Size(39, 13)
+        Me.lblMinimaUnidad.TabIndex = 0
+        Me.lblMinimaUnidad.Text = "Label1"
+        '
+        'cmdEjecutar
+        '
+        Me.cmdEjecutar.Location = New System.Drawing.Point(118, 12)
+        Me.cmdEjecutar.Name = "cmdEjecutar"
+        Me.cmdEjecutar.Size = New System.Drawing.Size(75, 23)
+        Me.cmdEjecutar.TabIndex = 1
+        Me.cmdEjecutar.Text = "Ejecutar"
+        Me.cmdEjecutar.UseVisualStyleBackColor = True
+        '
+        'txtIngresar
+        '
+        Me.txtIngresar.Location = New System.Drawing.Point(12, 12)
+        Me.txtIngresar.Name = "txtIngresar"
+        Me.txtIngresar.Size = New System.Drawing.Size(100, 20)
+        Me.txtIngresar.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtIngresar)
+        Me.Controls.Add(Me.cmdEjecutar)
+        Me.Controls.Add(Me.lblMinimaUnidad)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMinimaUnidad As Label
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents txtIngresar As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim numero, resto, suma As Integer
+        suma = 0
+        resto = 0
+        numero = txtIngresar.Text
+        Do While numero >= 10
+            suma = 0
+            Do While numero > 0
+                resto = numero Mod 10
+                suma = suma + resto
+                numero = (numero - resto) / 10
+            Loop
+            numero = suma
+        Loop
+        lblMinimaUnidad.Text = "La minima unidad es: " & numero
+    End Sub
+End Class
